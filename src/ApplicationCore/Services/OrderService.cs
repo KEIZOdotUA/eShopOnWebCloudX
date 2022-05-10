a01596aafe25f791c533658de83c1600072f1a70
Oleksii Konstantyniuk <oleksii_konstantyniuk@epam.com>
2022-05-10 16:48:00 +0300

    Final Assignment Added new packages; Implemented order uploading to 'reserver' function via Azure ServiceBus; Implemented order uploading to 'processor' function via Http; Updated appsettings; Updated Dockerfile for API project; Removed GitHub workflows; Added publish profiles;

--- src/ApplicationCore/Services/OrderService.cs
+++ src/ApplicationCore/Services/OrderService.cs
@@ -1,11 +1,17 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Ardalis.GuardClauses;
+using Azure.Messaging.ServiceBus;
 using Microsoft.eShopWeb.ApplicationCore.Entities;
 using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.ApplicationCore.Specifications;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace Microsoft.eShopWeb.ApplicationCore.Services;
 
@@ -15,16 +21,24 @@ public class OrderService : IOrderService
     private readonly IUriComposer _uriComposer;
     private readonly IRepository<Basket> _basketRepository;
     private readonly IRepository<CatalogItem> _itemRepository;
+    private readonly IConfiguration _configuration;
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    private const string _orderQueueName = "ok-queue";
 
     public OrderService(IRepository<Basket> basketRepository,
         IRepository<CatalogItem> itemRepository,
         IRepository<Order> orderRepository,
-        IUriComposer uriComposer)
+        IUriComposer uriComposer,
+        IConfiguration configuration,
+        IHttpClientFactory httpClientFactory)
     {
         _orderRepository = orderRepository;
         _uriComposer = uriComposer;
         _basketRepository = basketRepository;
         _itemRepository = itemRepository;
+        _configuration = configuration;
+        _httpClientFactory = httpClientFactory;
     }
 
     public async Task CreateOrderAsync(int basketId, Address shippingAddress)
@@ -49,5 +63,38 @@ public class OrderService : IOrderService
         var order = new Order(basket.BuyerId, shippingAddress, items);
 
         await _orderRepository.AddAsync(order);
+
+        await UploadToReserver(order);
+        await UploadToProcessor(order);
+    }
+
+    private async Task UploadToReserver(Order order)
+    {
+        var client = new ServiceBusClient(_configuration["ConnectionStrings:AzureSB"]);
+        var sender = client.CreateSender(_orderQueueName);
+        using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+        messageBatch.TryAddMessage(new ServiceBusMessage(JsonConvert.SerializeObject(order)));
+
+        try
+        {
+            await sender.SendMessagesAsync(messageBatch);
+        }
+        finally
+        {
+            await sender.DisposeAsync();
+            await client.DisposeAsync();
+        }
+    }
+
+    private async Task UploadToProcessor(Order order)
+    {
+        var client = _httpClientFactory.CreateClient("Azure Function");
+
+        var request = new HttpRequestMessage(HttpMethod.Post, String.Empty)
+        {
+            Content = new StringContent(JsonConvert.SerializeObject(order), Encoding.UTF8, "application/json")
+        };
+
+        await client.SendAsync(request);
     }
 }
